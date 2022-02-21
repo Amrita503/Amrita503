57e837692a3ba1abe56003e798985cc75fe7a5a4
Paudel Amrita <amupdl5@gmail.com>
2022-02-21 11:03:25 -0600

    added a new file

---
+++ Documents/Multiline comments.py
@@ -0,0 +1,19 @@
+#This is a multiline commenents
+# It is used many line comments in python 
+
+# Using many varibles and identifers python program is formated.
+
+""" We can also write by using triple quoted comments
+
+
+Hello my name is Amrita
+
+And Curently  I am living in Dallas tx
+
+"""
+
+Name ="Amrita"
+pirnt(Name)
+Age = 28
+print(type(Age))
+ 
